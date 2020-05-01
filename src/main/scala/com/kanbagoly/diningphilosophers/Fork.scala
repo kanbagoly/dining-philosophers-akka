b5e337a26920c0a51e0927ae0592969dd1bbeb9d
Kanbagoly <kanbagoly@gmail.com>
2020-05-01 07:05:16 +0200

    Simplify Fork

--- src/main/scala/com/kanbagoly/diningphilosophers/Fork.scala
+++ src/main/scala/com/kanbagoly/diningphilosophers/Fork.scala
@@ -16,17 +16,15 @@ object Fork {
   private val Free: Behavior[Command] = behavior(used = false)
   private val Used: Behavior[Command] = behavior(used = true)
 
-  private def behavior(used: Boolean): Behavior[Command] = Behaviors.receive { (context, message) =>
-    message match {
-      case PickUp(replyTo) =>
-        context.log.info("Pick up request from {}!", replyTo.ref)
-        replyTo ! Response(!used)
-        Used
-      case PutDown(replyTo) =>
-        context.log.info("Put down request from {}!", replyTo.ref)
-        replyTo ! Response(used)
-        Free
-    }
+  private def behavior(used: Boolean): Behavior[Command] = Behaviors.receive {
+    case (context, PickUp(replyTo)) =>
+      context.log.info("Pick up request from {}!", replyTo.ref)
+      replyTo ! Response(!used)
+      Used
+    case (context, PutDown(replyTo)) =>
+      context.log.info("Put down request from {}!", replyTo.ref)
+      replyTo ! Response(used)
+      Free
   }
 
 }

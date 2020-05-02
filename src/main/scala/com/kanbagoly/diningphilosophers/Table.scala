2efd0900ef9df8d8a9987f59dfae98cc17cc1748
Kanbagoly <kanbagoly@gmail.com>
2020-05-02 21:03:10 +0200

    Create more than one philosophers

--- src/main/scala/com/kanbagoly/diningphilosophers/Table.scala
+++ src/main/scala/com/kanbagoly/diningphilosophers/Table.scala
@@ -4,19 +4,25 @@ import akka.actor.typed._
 import akka.actor.typed.scaladsl.Behaviors
 
 object Table {
-  def apply(): Behavior[Nothing] =
+  def apply(numberOfPhilosophers: Int): Behavior[Nothing] = {
+    require(numberOfPhilosophers >= 2, "at least two philosophers need for the simulation")
     Behaviors.setup[Nothing] { context =>
       context.log.info("Eating philosophers simulation started")
-
-      val philosopher = context.spawn(Philosopher(), "philosopher")
-      context.watch(philosopher)
-
-      Behaviors.receiveSignal[Nothing] {
-        case (_, PostStop) =>
-          context.log.info("Eating philosophers simulation stopped")
-          Behaviors.same
-        case (_, Terminated(`philosopher`)) =>
-          Behaviors.stopped
+      (1 to numberOfPhilosophers).foreach { id =>
+        val philosopher = context.spawn(Philosopher(), s"philosopher-$id")
+        context.watch(philosopher)
       }
+      behavior(numberOfPhilosophers)
+    }
+  }
+
+  private def behavior(numberOfPhilosophers: Int): Behavior[Nothing] =
+    Behaviors.receiveSignal[Nothing] {
+      case (context, PostStop) =>
+        context.log.info("Eating philosophers simulation stopped")
+        Behaviors.same
+      case (context, t@Terminated(_)) =>
+        context.log.info("Actor {} terminated", t.ref)
+        if (numberOfPhilosophers > 1) behavior(numberOfPhilosophers - 1) else Behaviors.stopped
     }
 }

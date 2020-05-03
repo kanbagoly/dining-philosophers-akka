cb5d5501c84a2f1f70870293e41139aa75ab9699
Kanbagoly <kanbagoly@gmail.com>
2020-05-03 15:53:06 +0200

    Add timers to the Philosopher

--- src/main/scala/com/kanbagoly/diningphilosophers/Philosopher.scala
+++ src/main/scala/com/kanbagoly/diningphilosophers/Philosopher.scala
@@ -2,16 +2,27 @@ package com.kanbagoly.diningphilosophers
 
 import java.util.concurrent.atomic.AtomicInteger
 
+import akka.actor.typed.scaladsl.{Behaviors, TimerScheduler}
 import akka.actor.typed.{ActorRef, Behavior}
-import akka.actor.typed.scaladsl.Behaviors
 
 object Philosopher {
 
   sealed trait Command
-  final case object StartEating extends Command
+  final case object Eat extends Command
+  final case object Rest extends Command
 
-  def apply(leftFork: ActorRef[Fork.Command], rightFork: ActorRef[Fork.Command]): Behavior[Command] =
+  def apply(leftFork: ActorRef[Fork.Command],
+            rightFork: ActorRef[Fork.Command],
+            numberOfTimesToEat: Int = 5): Behavior[Command] = {
+  Behaviors.withTimers(timers => behavior(timers, leftFork, rightFork, numberOfTimesToEat)
+  )}
+
+  private def behavior(factory: TimerScheduler[Command],
+                       leftFork: ActorRef[Fork.Command],
+                       rightFork: ActorRef[Fork.Command],
+                       numberOfTimesToEat: Int): Behavior[Command] = {
     Behaviors.stopped
+  }
 
   private val NameCounter = new AtomicInteger(1)
 
